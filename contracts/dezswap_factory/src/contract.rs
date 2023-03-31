31edb7c76ffd451d6535494fe4cde7bd1d8e26ff
JoowonYun <joowon@delightlabs.io>
2023-03-31 20:46:50 +0900

    fix: target version

--- contracts/dezswap_factory/src/contract.rs
+++ contracts/dezswap_factory/src/contract.rs
@@ -388,7 +388,7 @@ pub fn query_native_token_decimal(
     Ok(NativeTokenDecimalsResponse { decimals })
 }
 
-const TARGET_CONTRACT_VERSION: &str = "1.1.1";
+const TARGET_CONTRACT_VERSION: &str = "1.1.0";
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> StdResult<Response> {
     migrate_version(
